b68d0d512586ea0f82b2669c9ddca718c10837a5
Kaindall <gustavozanete2@gmail.com>
2022-11-09 01:06:15 -0300

    Creation of all tables in SQL Code of the next activities

---
+++ eCommerce_Data-Insertion.sql
@@ -0,0 +1,6 @@
+INSERT INTO client (Cname, Csurname, cpf, phone, email, birthdate, street, s_number, city, state)
+VALUES	("Charles", "Oliveira", "11122233344", NULL, "charles@gmail.com", NULL, NULL, NULL, NULL, NULL),
+		("Giovanna", "Melo", "44433322211", NULL, "giovanna@gmail.com", NULL, NULL, NULL, NULL, NULL),
+        ("Everton", "Santos", "22233311144", NULL, "everton@hotmail.com", NULL, NULL, NULL, NULL, NULL),
+        ("Fabio", "Faria", "55544433322", NULL, "fabiof@hotmail.com", NULL, NULL, NULL, NULL, NULL);
+        
